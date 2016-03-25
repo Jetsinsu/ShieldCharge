f89744862511be39c497c582ce249e4bcc9d43a8
Jetsinsu <belinkster13@gmail.com>
2016-03-24 21:40:49 -0400

    Update ShieldChargeListener.java

--- src/me/jetsinsu/shieldcharge/ShieldChargeListener.java
+++ src/me/jetsinsu/shieldcharge/ShieldChargeListener.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.bukkit.Material;
 import org.bukkit.Sound;
+import org.bukkit.entity.Damageable;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
@@ -66,6 +67,7 @@ public class ShieldChargeListener implements Listener{
 	
 	public void shieldCharge(final Player p){
 		final int radius = plugin.getConfig().getInt("shieldcharge.radius");
+		final int damageamount = plugin.getConfig().getInt("shieldcharge.damage");
 		new BukkitRunnable(){
 			@Override
 			public void run() {
@@ -74,6 +76,7 @@ public class ShieldChargeListener implements Listener{
 				List<Entity> damaged = p.getNearbyEntities(radius, radius, radius);
 				for(Entity e: damaged){
 					if(e instanceof LivingEntity){
+						((Damageable) e).damage(damageamount);
 						e.setVelocity(p.getLocation().getDirection().setY(p.getLocation().getDirection().multiply(0.5).getY() + 0.5));
 						p.getWorld().playSound(p.getLocation(), Sound.ENTITY_ZOMBIE_ATTACK_DOOR_WOOD, 1, 1);
 					}
