1003fa2b5c0b5e29e88feccfa6c5aceae1fd9862
Jetsinsu <belinkster13@gmail.com>
2016-03-29 18:22:50 -0400

    Update ShieldCharge.java

--- src/me/jetsinsu/shieldcharge/ShieldCharge.java
+++ src/me/jetsinsu/shieldcharge/ShieldCharge.java
@@ -1,20 +1,16 @@
 package me.jetsinsu.shieldcharge;
 
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class ShieldCharge extends JavaPlugin{
+import CommandManager.Commands;
 
-	String sc = (ChatColor.DARK_GRAY + "[" + ChatColor.BLUE + "ShieldCharge" + ChatColor.DARK_GRAY + "]" + ChatColor.GREEN + " ");
-	String sc2 = (ChatColor.DARK_GRAY + "[" + ChatColor.BLUE + "ShieldCharge" + ChatColor.DARK_GRAY + "]" + ChatColor.RED + " ");
+public class ShieldCharge extends JavaPlugin{
 	
 	public void onEnable(){
 		Bukkit.getServer().getPluginManager().registerEvents(new ShieldChargeListener(this), this);
 		Bukkit.getServer().getPluginManager().registerEvents(new CustomDeathMessage(), this);
-		
+		getCommand("shieldcharge").setExecutor(new Commands(this));
 		getConfig().options().copyDefaults(true);
 		saveConfig();
 	}
@@ -22,59 +18,5 @@ public class ShieldCharge extends JavaPlugin{
 	public void onDisabled(){
 		saveConfig();
 	}
-	
-	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
-		if(cmd.getName().equalsIgnoreCase("shieldcharge")){
-			if(args.length == 0){
-				sender.sendMessage(sc + "/shieldcharge reload");
-				sender.sendMessage(sc + "/shieldcharge save");
-				return true;
-			}
-			
-			else if (args.length >= 1){
-				if(args[0].equalsIgnoreCase("reload")){
-					if(!(sender.hasPermission("sc.reload"))){
-						sender.sendMessage(sc2 + "You do not have permissions to use this command!");
-						return true;
-					}
-					
-					reloadConfig();
-					sender.sendMessage(sc + "ShieldCharge has been reloaded!");
-					return true;
-				}
-				
-				else if(args[0].equalsIgnoreCase("save")){
-					if(!(sender.hasPermission("sc.save"))){
-						sender.sendMessage(sc2 + "You do not have permissions to use this command!");
-						return true;
-					}
-					
-					saveConfig();
-					sender.sendMessage(sc + "ShieldCharge has been saved!");
-					return true;
-				}
-				
-				else if(args[0].equalsIgnoreCase("enable")){
-					if(getConfig().getBoolean("shieldcharge.enable")){
-						sender.sendMessage(sc2 + "The plugin is already enabled!");
-					}else{
-						getConfig().set("shieldcharge.enable", true);
-						sender.sendMessage(sc + "Plugin is now enabled!");
-					}
-					return true;
-				}
-				
-				else if(args[0].equalsIgnoreCase("disable")){
-					if(!getConfig().getBoolean("shieldcharge.enable")){
-						sender.sendMessage(sc2 + "The plugin is already disabled!");
-					}else{
-						getConfig().set("shieldcharge.enable", false);
-						sender.sendMessage(sc + "Plugin is now disabled!");
-					}
-					return true;
-				}
-			}
-		}
-		return true;
-	}
 }
+
